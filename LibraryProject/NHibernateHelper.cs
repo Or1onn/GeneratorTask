7b8f87622885edb5b6874b74fc910f8e64d02404
Orhan Salahetdinov <salaxetdinovorxan@gmail.com>
2024-06-13 17:33:47 +0400

    Initial commit

---
+++ LibraryProject/NHibernateHelper.cs
@@ -0,0 +1,35 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Mapping.ByCode;
+using System.Reflection;
+
+namespace LibraryProject;
+
+public class NHibernateHelper
+{
+    private static ISessionFactory _sessionFactory;
+
+    private static ISessionFactory SessionFactory
+    {
+        get
+        {
+            if (_sessionFactory == null)
+            {
+                var configuration = new Configuration();
+                configuration.Configure(); // Использует hibernate.cfg.xml
+                var mapper = new ModelMapper();
+                var assembly = Assembly.GetExecutingAssembly();
+                mapper.AddMappings(assembly.GetExportedTypes());
+                configuration.AddMapping(mapper.CompileMappingForAllExplicitlyAddedEntities());
+                _sessionFactory = configuration.BuildSessionFactory();
+            }
+
+            return _sessionFactory;
+        }
+    }
+
+    public static ISession OpenSession()
+    {
+        return SessionFactory.OpenSession();
+    }
+}
---
+++ SourceGenerator/DatabaseClassGenerator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Npgsql;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+using System.Text;
+
+
+namespace SourceGenerator;
+
+public class DatabaseClassGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var connectionString = "Host=localhost;Database=teestdb;Username=postgres;Password=admin";
+        var classes = GetClassNamesFromDatabase(connectionString);
+
+        foreach (var className in classes)
+        {
+            var source = GenerateClassSource(className);
+            context.AddSource($"{className}.g.cs", SourceText.From(source, Encoding.UTF8));
+        }
+    }
+
+    private List<string> GetClassNamesFromDatabase(string connectionString)
+    {
+        using var connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+        var compiler = new PostgresCompiler();
+        var db = new QueryFactory(connection, compiler);
+
+        var classNames = db.Query("ClassNames").Select("ClassName").Get<string>().ToList();
+        return classNames;
+    }
+
+    private string GenerateClassSource(string className)
+    {
+        var source = $@"
+            namespace GeneratedClasses
+            {{
+                public class {className}
+                {{
+                    public long Id {{ get; set; }}
+                }}
+            }}";
+        return source;
+    }
+}
